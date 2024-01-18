fd98edf8a055ed8d714f5ad772295a9f1e1694a1
LauraKuzara <kuzaralaura@gmail.com>
2024-01-18 15:08:52 -0500

    completed upto step 7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+let currentTemp = 60;
+
+let isRaining = true;
+
+if (currentTemp <= 40) {
+    console.log(`It’s ${currentTemp} degrees outside! Wear a heavy jacket.`);
+}
+
+else if (currentTemp <= 60) {
+    console.log(`It’s ${currentTemp} degrees outside! Wear a jacket.`);
+}
+
+else if (currentTemp < 70) {
+    console.log(`It’s ${currentTemp} degrees outside! Wear a light jacket.`);
+}
+
+else {
+    console.log("It's hot out today! No jacekt needed!");
+}
+
