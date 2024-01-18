3610d333325169af2b5a8734895e2f344552bc56
LauraKuzara <kuzaralaura@gmail.com>
2024-01-18 15:12:46 -0500

    completed up to step 9

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("Hello World!");
 
-let currentTemp = 60;
+let currentTemp = 32;
 
-let isRaining = true;
+let isRaining = false;
 
 if (currentTemp <= 40) {
     console.log(`It’s ${currentTemp} degrees outside! Wear a heavy jacket.`);
@@ -20,3 +20,9 @@ else {
     console.log("It's hot out today! No jacekt needed!");
 }
 
+if (isRaining) {
+    console.log("It’s raining, too, so you should bring an umbrella!"); 
+}
+else {
+    console.log("No rain today, you can leave the umbrella at home!");
+}
